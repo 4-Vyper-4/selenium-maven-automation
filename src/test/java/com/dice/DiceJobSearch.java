04ac7053ce49fde8602e94faa563c18ece46525e
4-Vyper-4 <charles.s.cybertek@gmail.com>
2018-06-13 20:13:48 -0400

    add case read from file

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,11 @@
 package com.dice;
 
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -10,6 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DiceJobSearch {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		// maven webdriver depend setup
@@ -21,7 +28,7 @@ public class DiceJobSearch {
 		driver.manage().window().fullscreen();
 
 		// set wait time, universally
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 		// open webpage
 		driver.get("https://www.dice.com/");
@@ -29,6 +36,7 @@ public class DiceJobSearch {
 		// check title
 		String expected = "Job Search for Technology Professionals | Dice.com";
 		String actual = driver.getTitle();
+		String holder = new String();
 
 		if (actual.equals(expected))
 			System.out.println("Pass");
@@ -39,9 +47,29 @@ public class DiceJobSearch {
 			throw new RuntimeException("Step FAIL: dice hompage did not load succesfully");
 		}
 
+		String titles = "C:\\Users\\Vyper\\Documents\\Maven\\selenium-maven-automation\\job_titles.txt";
+		String line = null;
+		ArrayList<String> tl = new ArrayList<>();
+		ArrayList<String> tlResult = new ArrayList<>();
+
+		try {
+			FileReader fileReader = new FileReader(titles);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while ((line = bufferedReader.readLine()) != null) {
+				tl.add(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file '" + titles + "'");
+			e.printStackTrace();
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + titles + "'");
+
+		}
+
+		for(int i = 0; i < tl.size(); i++) {
 		// create object of word we search
-		String keyword = "sdet";
-		// locate search element of search //send key to input text
+		String keyword = (String) tl.get(i);
+				// locate search element of search //send key to input text
 		driver.findElement(By.id("search-field-keyword")).clear();
 		driver.findElement(By.id("search-field-keyword")).sendKeys(keyword);
 
@@ -60,15 +88,23 @@ public class DiceJobSearch {
 		// to check count more than one
 		int countResult = Integer.parseInt(count.replace(",", ""));
 
-		if (countResult > 0)
+		if (countResult > 0) {
 			System.out
 					.println("PASS: Keyword " + keyword + " search results are " + countResult + " in location " + zip);
-		else {
+		holder = keyword.concat((" "+countResult));
+		}else {
 			System.out
 					.println("FAIL: Keyword " + keyword + " search results are " + countResult + " in location " + zip);
+		continue;
 		}
-
+		tlResult.add(holder);
+		driver.navigate().back();
+		
+		}
+		
+		
 		driver.close();
+		System.out.println(tlResult);
 	}
 
 }
